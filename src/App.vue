d671e30f610fe15cff6fae186f0cfd4cabab816f
OHMORIYUSUKE <ohmoriyusuke@gmail.com>
2021-06-09 19:03:44 +0900

    make view

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,58 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+<v-app-bar
+      fixed
+      elevate-on-scroll
+      color="white"
+      scroll-target="#scrolling-techniques-7"
+    >
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-toolbar-title>MyTube</v-toolbar-title>
+
+      <v-spacer>
+      </v-spacer>
+
+      <v-text-field
+      class="text"
+        v-model="searchText"
+        hide-details single-line
+        outlined></v-text-field>
+
+    <v-btn depressed>
+      <v-icon>mdi-magnify</v-icon>
+    </v-btn>
+
+      <v-spacer>
+      </v-spacer>
+
+    </v-app-bar>
+
+    <div class="maincontent">
+      <HelloWorld/>
+    </div>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld';
 
 export default {
   name: 'App',
+
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+
+  data: () => ({
+    //
+  }),
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+.maincontent {
+  padding-top: 70px;
+  background-color: rgb(245, 245, 245);
 }
 </style>
